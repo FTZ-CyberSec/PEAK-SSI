9f96f95f4ccf57daf131a81dcacc4088b916bcb1
MoritzVolkman <80387757+MoritzVolkman@users.noreply.github.com>
2024-04-16 15:28:47 +0200

    Added functionality to run docker without sudo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,7 @@
 pip install aries-cloudagent
 git clone https://github.com/bcgov/indy-tails-server.git
 git clone https://github.com/bcgov/von-network.git
+sudo groupadd docker
+sudo usermod -aG docker $USER
 indy-tails-server/docker/manage up
 von-network/manage up
