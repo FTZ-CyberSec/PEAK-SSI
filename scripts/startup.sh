1412c4d0d8a540de006588bdfaac2d582389aab1
acb017 <moeserdkunde@gmail.com>
2024-04-17 10:03:24 +0200

    Added DID registration to startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,37 @@
 ~/indy-tails-server/docker/manage start
 ~/von-network/manage start
-echo "If this is your first startup, go to the server-adress port 9000 and register the relevant DIDs"
+URL="http://82.165.247.238:9000/register"
+PAYLOAD1='{
+  "role": "ENDORSER",
+  "alias": null,
+  "did": null,
+  "seed": "Platform000000000000000000000000"
+}'
+PAYLOAD2='{
+  "role": "ENDORSER",
+  "alias": null,
+  "did": null,
+  "seed": Grid0000000000000000000000000000"
+  }'
+PAYLOAD3='{
+  "role": "ENDORSER",
+  "alias": null,
+  "did": null,
+  "seed": Prosumer000000000000000000000001"
+}'
+curl -X POST \
+     -H "Content-Type: application/json" \
+     -d "$PAYLOAD1" \
+     "$URL"
+curl -X POST \
+      -H "Content-Type: application/json" \
+      -d "$PAYLOAD2" \
+      "$URL"
+curl -X POST \
+      -H "Content-Type: application/json" \
+      -d "$PAYLOAD3" \
+      "$URL"
+
 nohup bash platform.sh &
 nohup bash grid.sh &
 nohup bash 1prosumer.sh &
